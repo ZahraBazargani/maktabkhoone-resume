34fa53b2b74a844687ed67f835bb6e9fc79186fa
ZahraBazargani <zhrbz76@gmail.com>
2020-06-19 20:04:08 +0430

    Updates

---
+++ precache-manifest.5662c88ce522e96093f4725b5a17d971.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10391592692ee907c03dcb0fdf46b879",
+    "url": "/maktabkhoone-resume/index.html"
+  },
+  {
+    "revision": "b898b22a6d8ce1a893ca",
+    "url": "/maktabkhoone-resume/static/css/main.b87b14ec.chunk.css"
+  },
+  {
+    "revision": "b38cdfa28f2533546bd9",
+    "url": "/maktabkhoone-resume/static/js/2.3d0913ac.chunk.js"
+  },
+  {
+    "revision": "e4d0b7b6ee221c9758b548593a6bc2ac",
+    "url": "/maktabkhoone-resume/static/js/2.3d0913ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b898b22a6d8ce1a893ca",
+    "url": "/maktabkhoone-resume/static/js/main.1a2c6c5e.chunk.js"
+  },
+  {
+    "revision": "dbc0d686f6d8d066ec83",
+    "url": "/maktabkhoone-resume/static/js/runtime-main.85773505.js"
+  }
+]);
